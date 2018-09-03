bd351e3fd0015d7d3c773ecdbe077961d39e4798
Trevor Vincent <trevor-vincent@users.noreply.github.com>
2018-09-03 18:49:46 -0400

    Update twitter_sentiment.py
    First draft version of twitter_sentiment

--- MachineLearning/SentimentAnalysis/twitter_sentiment.py
+++ MachineLearning/SentimentAnalysis/twitter_sentiment.py
@@ -1,1 +1,30 @@
+import tweepy
+from textblob import TextBlob
 
+#Step 1 - Authenticate
+consumer_key = ''
+consumer_secret = ''
+
+access_token=''
+access_token_secret=''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_Access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+
+#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+#and label each one as either 'positive' or 'negative', depending on the sentiment 
+#You can decide the sentiment polarity threshold yourself
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+    
+    #Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print("")
